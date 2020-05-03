c6b15b573abbe45a3620f3e9dc8204bb1a96c85a
Joel <joeldmyers@gmail.com>
2020-05-03 11:45:20 -0400

    adding sequencer lights to the buttons

--- src/components/SequencerButton/SequencerButton.js
+++ src/components/SequencerButton/SequencerButton.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./SequencerButton.scss";
 
 const SequencerButton = (props) => {
+  const { active } = props;
+
   return (
     <button className="dm-sequencer-btn" style={{ width: props.buttonWidth }}>
       {props.selected && <span className="dm-sequencer-selected"></span>}
+      <span
+        className={`dm-sequencer-btn-light ${props.active ? "active" : ""}`}
+      >
+        &#9673;
+      </span>
     </button>
   );
 };
