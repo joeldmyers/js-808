3b9420299a857af2f5c28196586d3ca6fe219adf
Joel <joeldmyers@gmail.com>
2020-05-04 20:26:05 -0400

    server.js fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 8080;
 const path = require("path");
 const app = express();
 
-app.use("/", express.static("public"));
+// app.use("/", express.static("public"));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.listen(port);
