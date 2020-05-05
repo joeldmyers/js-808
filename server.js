847ac17666f0e4ff0dfe3d400159cd7bd47ff9a3
Joel <joeldmyers@gmail.com>
2020-05-04 20:01:44 -0400

    node server tweak

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static("__dirname"));
-
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + "public"));
 
 app.listen(port);
