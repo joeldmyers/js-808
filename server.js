eb91150e87856ef4bf19f7cdac5ac31a3a17f758
Joel <joeldmyers@gmail.com>
2020-05-04 20:24:04 -0400

    server.js fix

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ const port = process.env.PORT || 8080;
 const path = require("path");
 const app = express();
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static("public"));
-
-// send the user to index html page inspite of the url
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("public/index.html"));
-});
+app.use("/", express.static("public"));
 
 app.listen(port);
